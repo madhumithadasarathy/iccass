cc57e72ffd45e12577bcea4d4b90e5bbe33440cb
madhumithadasarathy <110802087+madhumithadasarathy@users.noreply.github.com>
2025-11-03 19:51:10 +0530

    Set base URL and output directory for build
    Signed-off-by: madhumithadasarathy <110802087+madhumithadasarathy@users.noreply.github.com>

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',           // important for iccass2026.in (root domain)
+  build: {
+    outDir: 'dist',    
+  },
 })
